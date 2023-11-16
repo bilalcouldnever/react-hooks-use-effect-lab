06ff424814690e4292f93da63cedfa8a7e0866ec
bilalcouldnever <bilal.faruq@student.moringaschool.com>
2023-11-16 12:23:29 +0300

    done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
-function Question({ question, onAnswered }) {
+const Question = ({ question, onAnswered }) => {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // Set up a timer using setTimeout
+    const timer = setTimeout(() => {
+      // Decrease the time remaining by 1 every second
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
 
+    // Cleanup function for useEffect
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining]); // Dependency array to avoid warnings
+
+  useEffect(() => {
+    // Check if timeRemaining is 0
+    if (timeRemaining === 0) {
+      // Reset timeRemaining to 10 seconds for the next question
+      setTimeRemaining(10);
+
+      // Trigger behavior in the App component by calling onAnswered with false
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]); // Dependency array to avoid warnings
+  
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
@@ -28,5 +50,6 @@ function Question({ question, onAnswered }) {
     </>
   );
 }
+
 
 export default Question;
